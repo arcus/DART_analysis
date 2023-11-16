d368cd555a9c50d9bc3d94af4624a1eb8029b3e5
rosemm <hartmanr1@chop.edu>
2023-11-16 13:04:13 -0500

    Save theme and chop colors in functions_plotting.R

---
+++ src/scripts/functions_plotting.R
@@ -0,0 +1,9 @@
+library(ggplot2)
+theme_set(theme_classic())
+
+chop_blue <- "#41b6e6"
+chop_darkblue <- "#005587"
+chop_pink <- "#ed1f7f"
+chop_green <- "#91a01e"
+chop_brown <- "#786452"
+chop_brown_text <- "#55473c"
