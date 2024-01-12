cb17c3798bd1ead2d4f8e0c949b3b93a77c045c8
rosemm <hartmanr1@chop.edu>
2024-01-12 12:25:08 -0500

    Improve comments

--- src/scripts/03_clean_needs_assessment.R
+++ src/scripts/03_clean_needs_assessment.R
@@ -5,9 +5,9 @@ readRDS(here::here("data", "raw", "DART_Pipeline.rds")) |>
   dplyr::filter(needs_assessment_complete == 2) |> 
   # select just fields from needs assessment
   dplyr::select(record_id, data_viz_relevance:longitudinal_learn) |> 
-  # 6 = "not sure", make missing on all likert columns
+  # 6 = "not sure", make missing on all likert columns (the function not_sure is defined in functions_data_cleaning.R)
   dplyr::mutate(dplyr::across(tidyselect::matches("_relevance|_expertise|_learn"), not_sure)) |> 
-  # replace all missing values with 1 (the lowest possible score)
+  # replace all missing values with 1 (the lowest possible score) (the function impute_missing_as_1 is defined in functions_data_cleaning.R)
   dplyr::mutate(dplyr::across(tidyselect::matches("_relevance|_expertise|_learn"), impute_missing_as_1)) |> 
   # ensure there's no potentially identifying information (names, emails, open text fields)
   dplyr::select(!tidyselect::where(is.character)) |> 
