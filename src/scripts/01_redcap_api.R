39af89d52663dd95b255153143a9fdf7775d2e0f
rosemm <hartmanr1@chop.edu>
2025-07-23 16:42:24 -0400

    Add an API call to get metadata (question text) for Pipeline fields

--- src/scripts/01_redcap_api.R
+++ src/scripts/01_redcap_api.R
@@ -25,6 +25,26 @@ if(is.null(nrow(pipeline_result))) {
   saveRDS(pipeline_result, file = here::here("data", "raw", "DART_Pipeline.rds"))
 }
 
+# get the metadata for Pipeline fields
+metadata_formData <- list("token"=Sys.getenv("Pipeline_56668"),
+                          content='metadata',
+                          format='json',
+                          returnFormat='json'
+)
+metadata_response <- httr::POST("https://redcap.chop.edu/api/", body = metadata_formData, encode = "form")
+metadata_result <- httr::content(metadata_response, show_col_types = FALSE) |> 
+  # convert the list to a dataframe of metadata by field
+  purrr::map(magrittr::extract, c("form_name", "field_name", "field_label", "field_type", "matrix_group_name", "section_header")) |> 
+  dplyr::bind_rows()
+
+if(is.null(nrow(metadata_result))) {
+  message("\nTHE PIPELINE METADATA DID NOT DOWNLOAD\n")
+  print(metadata_result$error)
+} else {
+  # save raw data
+  saveRDS(metadata_result, file = here::here("data", "raw", "DART_Pipeline_metadata.rds"))
+}
+
 # pull data from NALMS
 nalms_formData <- list("token"=Sys.getenv("NALMS_Wave2_61127"),
                  content='record',
